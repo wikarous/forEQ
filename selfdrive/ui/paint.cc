1768e5268e4bd5433072325730e3f89587d94a99
chanhojung <jungch@naver.com>
2020-10-25 19:40:13 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -599,7 +599,7 @@ static void ui_draw_debug(UIState *s)
     ui_print( s, ui_viz_rx, ui_viz_ry+200, "aD:%.2f", scene.pathPlan.steerActuatorDelay );
     ui_print( s, ui_viz_rx, ui_viz_ry+250, "sF:%.2f", scene.liveParams.stiffnessFactor );
     ui_print( s, ui_viz_rx, ui_viz_ry+750, "LeftLane(%%) LeftPoly(%%)  LaneWidth  RightPoly(%%) RightLane(%%)");
-    ui_print( s, ui_viz_rx, ui_viz_ry+800, "       %5.1f               %4.1f                  %4.2f                  %4.1f               %5.1f", 
+    ui_print( s, ui_viz_rx, ui_viz_ry+800, "       %5.1f                %4.1f                   %4.2f                   %4.1f               %5.1f", 
       scene.pathPlan.lProb*100, (scene.pathPlan.lPoly/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, scene.pathPlan.rProb*100 ); 
 
   }
