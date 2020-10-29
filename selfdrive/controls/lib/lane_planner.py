ad7e22974cbc46c7facc7ef272484f53ead2fd29
chanhojung <jungch@naver.com>
2020-10-29 15:50:03 +0900

    Update lane_planner.py

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -49,8 +49,10 @@ def calc_d_poly(l_poly, r_poly, p_poly, l_prob, r_prob, lane_width, v_ego):
   # elif lr_prob > 0.575 and l_prob > 0.2 and r_prob > 0.2 :
   #   lr_prob = max(0.875, lr_prob)
   # neokii
-  #if lr_prob > 0.65:
-  #  lr_prob = min(lr_prob * 1.35, 1.0)
+  if lr_prob > 0.7:
+    lr_prob = min(lr_prob * 1.34, 1.0)
+  elif lr_prob > 0.36:
+    lr_prob = min(lr_prob * 1.625, 0.93)
 
   d_poly_lane = (l_prob * path_from_left_lane + r_prob * path_from_right_lane) / (l_prob + r_prob + 0.0001)
   return lr_prob * d_poly_lane + (1.0 - lr_prob) * p_poly
